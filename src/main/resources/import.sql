f75a19da87cae7b72200af97dc3b30dc06d989c7
Gabriel Montes <gabriel_montesvzla@hotmail.com>
2021-08-25 15:49:48 -0600

    Update import.sql
    se añadieron más datos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,5 @@
 INSERT INTO clientes ( nombres, apellidos, email) VALUES ( 'gabriel', 'Montes', 'gabriel@hotmail.com');
+INSERT INTO clientes ( nombres, apellidos, email) VALUES ( 'Jose', 'Valenzuela', 'jose@hotmail.com');
+INSERT INTO clientes ( nombres, apellidos, email) VALUES ( 'Rey', 'Perez', 'rey@hotmail.com');
+INSERT INTO clientes ( nombres, apellidos, email) VALUES ( 'Luis', 'Lopez', 'luis@hotmail.com' );
+INSERT INTO clientes ( nombres, apellidos, email) VALUES ( 'Luz', 'Pena', 'luz@hotmail.com');
