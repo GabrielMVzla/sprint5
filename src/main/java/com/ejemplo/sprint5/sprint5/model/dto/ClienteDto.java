8b715914187e0271a4e9a9bd6270d84648152784
GabrielMVzla <gabriel_montesvzla@hotmail.com>
2021-09-02 12:43:17 -0600

    Update ClienteDto.java

--- src/main/java/com/ejemplo/sprint5/sprint5/model/dto/ClienteDto.java
+++ src/main/java/com/ejemplo/sprint5/sprint5/model/dto/ClienteDto.java
@@ -7,15 +7,12 @@ import javax.validation.constraints.Pattern;
 
 
 @Data
-public class ClienteDto {
-
+public class ClienteDto 
+{
     @NotBlank(message = "no debe estar vacío")
     private String nombres, apellidos;
 
     @NotBlank(message = "no debe estar vacío")
     @Pattern(regexp = "[\\w ]{1,10}", message = "debe tener una longitud máxima de 10, solo permite letras")
     private String direccion;
-
-
-
 }
